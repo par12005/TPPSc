b2f40f2e648f388a75bc46d1c49224c45de8128a
Vlad Savitsky <vlad.savitsky@gmail.com>
2023-08-15 17:37:48 +0300

    #861n70hh6 Removed outdated AJAX-callback function 'tppsc_organism_callback()'.

--- forms/build/page_1_ajax.php
+++ forms/build/page_1_ajax.php
@@ -33,10 +33,3 @@ function tppsc_ajax_doi_callback(&$form, $form_state) {
   return $form;
 }
 
-/**
- *
- */
-function tppsc_organism_callback($form, &$form_state) {
-
-  return $form['organism'];
-}
